093e5f19bf0456289cec1cd3f8305769bee93ba8
KhaiVpn767 <112326124+KhaiVpn767@users.noreply.github.com>
2023-08-31 23:21:50 +0800

    Add files via upload

---
+++ delete-user/del-ssh.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#wget https://github.com/${GitUser}/
+GitUser="KhaiVpn767"
+#IZIN SCRIPT
+MYIP=$(curl -sS ipv4.icanhazip.com)
+clear
+read -p "Username SSH to Delete : " Pengguna
+
+if getent passwd $Pengguna > /dev/null 2>&1; then
+        userdel $Pengguna
+        echo -e "User $Pengguna was removed."
+else
+        echo -e "Failure: User $Pengguna Not Exist."
+fi
