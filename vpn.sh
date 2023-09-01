222e13d8117e51d034a32fd0be9e9697451a44cc
KhaiVpn767 <112326124+KhaiVpn767@users.noreply.github.com>
2023-09-02 06:21:05 +0800

    Add files via upload

--- vpn.sh
+++ vpn.sh
@@ -16,7 +16,7 @@ apt install openvpn easy-rsa unzip -y
 apt install openssl iptables iptables-persistent -y
 mkdir -p /etc/openvpn/server/easy-rsa/
 cd /etc/openvpn/
-wget https://raw.githubusercontent.com/${GitUser}/t-code/main/vpn.zip
+wget https://raw.githubusercontent.com/${GitUser}/multiport/main/vpn.zip
 unzip vpn.zip
 rm -f vpn.zip
 chown -R root:root /etc/openvpn/server/easy-rsa/
