d7082bf1bc2f184c6a2ce4080b08d900de8853d8
KhaiVpn767 <112326124+KhaiVpn767@users.noreply.github.com>
2023-09-19 22:22:42 +0800

    Update ins-xray.sh

--- install/ins-xray.sh
+++ install/ins-xray.sh
@@ -551,7 +551,7 @@ cat> /usr/local/etc/xray/trojan.json << END
                 ],
                 "fallbacks": [
                     {
-                        "dest": 8118
+                        "dest": 8888
                     }
                 ]
             },
