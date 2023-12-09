30304ba8765bb0519e9fae66e90215101be55910
KhaiVpn767 <112326124+KhaiVpn767@users.noreply.github.com>
2023-12-09 18:38:06 +0800

    Update system.sh

--- menu/system.sh
+++ menu/system.sh
@@ -37,6 +37,8 @@ echo -e "   \e[$number (11)\e[m \e[$below Install BBR\e[m"
 echo -e "   \e[$number (12)\e[m \e[$below ON/OF Auto Reboot\e[m"
 echo -e "   \e[$number (13)\e[m \e[$below Change Password VPS\e[m"
 echo -e "   \e[$number (14)\e[m \e[$below Install UDP Custom\e[m"
+echo -e "   \e[$number (15)\e[m \e[$below Install SlowDNS\e[m"
+echo -e "   \e[$number (16)\e[m \e[$below Swap RAM\e[m"
 echo -e ""
 echo -e "   \e[$line════════════════════════════════════════\e[m"
 echo -e "   \e[$back_text \e[$box x)   MENU                             \e[m"
@@ -85,7 +87,13 @@ autoreboot
 passwd
 ;;
 14)
-wget https://raw.githubusercontent.com/NevermoreSSH/Vergil/main/Tunnel/udp.sh && bash udp.sh
+wget https://raw.githubusercontent.com/KhaiVpn767/MultiportV3/main/Tunnel/udp.sh && bash udp.sh
+;;
+15)
+wget https://raw.githubusercontent.com/KhaiVpn767/MultiportV3/main2/addons/dns2.sh && chmod +x dns2.sh && ./dns2.sh
+;;
+16)
+wget -q -O /usr/bin/swapram "https://raw.githubusercontent.com/KhaiVpn767/swapram/main/swapram.sh" && chmod +x /usr/bin/swapram && swapram
 ;;
 x)
 menu
