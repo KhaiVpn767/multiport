d5cd6a7cd4a87cf484d149b60cee9580aa78f348
KhaiVpn767 <112326124+KhaiVpn767@users.noreply.github.com>
2024-01-03 05:43:45 +0800

    Update system.sh

--- menu/system.sh
+++ menu/system.sh
@@ -38,6 +38,7 @@ echo -e "   \e[$number (12)\e[m \e[$below Change Password VPS\e[m"
 echo -e "   \e[$number (13)\e[m \e[$below SwapRAM Menu \e[m"
 echo -e "   \e[$number (14)\e[m \e[$below Install UDP Custom\e[m"
 echo -e "   \e[$number (15)\e[m \e[$below Install SlowDNS\e[m"
+echo -e "   \e[$number (16)\e[m \e[$below XRAY-CORE MENU\e[m"
 echo -e ""
 echo -e "   \e[$line════════════════════════════════════════\e[m"
 echo -e "   \e[$back_text \e[$box x)   MENU                             \e[m"
@@ -90,6 +91,9 @@ wget https://raw.githubusercontent.com/NevermoreSSH/Vergil/main/Tunnel/udp.sh &&
 ;;
 15)
 wget https://raw.githubusercontent.com/NevermoreSSH/Vergil/main2/addons/dns2.sh && chmod +x dns2.sh && ./dns2.sh
+;;
+16)
+wget https://raw.githubusercontent.com/KhaiVpn767/multiport/main/xraychanger.sh && chmod +x xraychanger.sh && ./xraychanger.sh
 ;;
 x)
 menu
