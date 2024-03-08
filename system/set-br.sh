51eb9b62e6558e08d7d13237d93507aa7c019af9
KhaiVpn767 <112326124+KhaiVpn767@users.noreply.github.com>
2024-03-08 18:11:43 +0800

    Add files via upload

--- system/set-br.sh
+++ system/set-br.sh
@@ -1,62 +1,72 @@
 #!/bin/bash
-# =========================================
-# Quick Setup | Script Setup Manager
-# Edition : Stable Edition V1.0
-# Auther  : KhaiVpn767
-# (C) Copyright 2022
-# =========================================
-export Server_URL="raw.githubusercontent.com/NevermoreSSH/yourpath/main"
-
-clear
-dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
-biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
-#########################
-MYIP=$(wget -qO- ipv4.icanhazip.com);
-MYIP=$(curl -s ipinfo.io/ip )
+#Optimasi Speed By comingsoon
+#wget https://github.com/${GitUser}/
+GitUser="KhaiVpn767"
+#IZIN SCRIPT
 MYIP=$(curl -sS ipv4.icanhazip.com)
-MYIP=$(curl -sS ifconfig.me )
 clear
-red='\e[1;31m'
-green='\e[0;32m'
-yell='\e[1;33m'
-tyblue='\e[1;36m'
-purple='\e[0;35m'
-NC='\e[0m'
-purple() { echo -e "\\033[35;1m${*}\\033[0m"; }
-tyblue() { echo -e "\\033[36;1m${*}\\033[0m"; }
-yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
-green() { echo -e "\\033[32;1m${*}\\033[0m"; }
-red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+Add_To_New_Line(){
+	if [ "$(tail -n1 $1 | wc -l)" == "0"  ];then
+		echo "" >> "$1"
+	fi
+	echo "$2" >> "$1"
+}
 
-red='\e[1;31m'
-green='\e[0;32m'
-NC='\e[0m'
-green() { echo -e "\\033[32;1m${*}\\033[0m"; }
-red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+Check_And_Add_Line(){
+	if [ -z "$(cat "$1" | grep "$2")" ];then
+		Add_To_New_Line "$1" "$2"
+	fi
+}
 
-#curl https://rclone.org/install.sh | bash
-apt install rclone
-printf "q\n" | rclone config
-wget -O /root/.config/rclone/rclone.conf "https://${Server_URL}/rclone.conf"
-git clone  https://github.com/MrMan21/wondershaper.git
-cd wondershaper
-make install
-cd
-rm -rf wondershaper
-cd /usr/bin
-wget -O backup "https://${Server_URL}/backup.sh"
-wget -O restore "https://${Server_URL}/restore.sh"
-wget -O cleaner "https://${Server_URL}/logcleaner.sh"
-chmod +x /usr/bin/backup
-chmod +x /usr/bin/restore
-chmod +x /usr/bin/cleaner
-cd
-if [ ! -f "/etc/cron.d/cleaner" ]; then
-cat> /etc/cron.d/cleaner << END
-SHELL=/bin/sh
-PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-*/2 * * * * root /usr/bin/cleaner
-END
+Install_BBR(){
+echo -e "\e[32;1m================================\e[0m"
+echo -e "\e[32;1mInstall TCP BBR...\e[0m"
+if [ -n "$(lsmod | grep bbr)" ];then
+echo -e "\e[0;32mSuccesfully Installed TCP BBR.\e[0m"
+echo -e "\e[32;1m================================\e[0m"
+return 1
 fi
-service cron restart > /dev/null 2>&1
-rm -f /root/set-br.sh
+echo -e "\e[0;32mMulai menginstall TCP_BBR...\e[0m"
+modprobe tcp_bbr
+Add_To_New_Line "/etc/modules-load.d/modules.conf" "tcp_bbr"
+Add_To_New_Line "/etc/sysctl.conf" "net.core.default_qdisc = fq"
+Add_To_New_Line "/etc/sysctl.conf" "net.ipv4.tcp_congestion_control = bbr"
+sysctl -p
+if [ -n "$(sysctl net.ipv4.tcp_available_congestion_control | grep bbr)" ] && [ -n "$(sysctl net.ipv4.tcp_congestion_control | grep bbr)" ] && [ -n "$(lsmod | grep "tcp_bbr")" ];then
+	echo -e "\e[0;32mTCP_BBR Install Success.\e[0m"
+else
+	echo -e "\e[1;31mGagal menginstall TCP_BBR.\e[0m"
+fi
+echo -e "\e[32;1m================================\e[0m"
+}
+
+Optimize_Parameters(){
+echo -e "\e[32;1m================================\e[0m"
+echo -e "\e[32;1mOptimasi Parameters...\e[0m"
+Check_And_Add_Line "/etc/security/limits.conf" "* soft nofile 51200"
+Check_And_Add_Line "/etc/security/limits.conf" "* hard nofile 51200"
+Check_And_Add_Line "/etc/security/limits.conf" "root soft nofile 51200"
+Check_And_Add_Line "/etc/security/limits.conf" "root hard nofile 51200"
+Check_And_Add_Line "/etc/sysctl.conf" "fs.file-max = 51200"
+Check_And_Add_Line "/etc/sysctl.conf" "net.core.rmem_max = 67108864"
+Check_And_Add_Line "/etc/sysctl.conf" "net.core.wmem_max = 67108864"
+Check_And_Add_Line "/etc/sysctl.conf" "net.core.netdev_max_backlog = 250000"
+Check_And_Add_Line "/etc/sysctl.conf" "net.core.somaxconn = 4096"
+Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.tcp_syncookies = 1"
+Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.tcp_tw_reuse = 1"
+Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.tcp_fin_timeout = 30"
+Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.tcp_keepalive_time = 1200"
+Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.ip_local_port_range = 10000 65000"
+Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.tcp_max_syn_backlog = 8192"
+Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.tcp_max_tw_buckets = 5000"
+Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.tcp_fastopen = 3"
+Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.tcp_mem = 25600 51200 102400"
+Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.tcp_rmem = 4096 87380 67108864"
+Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.tcp_wmem = 4096 65536 67108864"
+Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.tcp_mtu_probing = 1"
+echo -e "\e[0;32mSuccesfully Optimize Parameters.\e[0m"
+echo -e "\e[32;1m================================\e[0m"
+}
+Install_BBR
+Optimize_Parameters
+rm -f /root/bbr.sh
