61c5941d65dd6ad93a92349e78b31ebc1f6bc892
KhaiVpn767 <112326124+KhaiVpn767@users.noreply.github.com>
2024-08-23 01:24:01 +0800

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -140,9 +140,8 @@ echo -e   "  \e[$number (•3)\e[m \e[$below OPENSSH & OPENVPN\e[m           \e[
 echo -e   "  \e[$number (•4)\e[m \e[$below PANEL SHADOWSOCKS\e[m           \e[$number (10)\e[m \e[$below CHECK RUNNING\e[m"
 echo -e   "  \e[$number (•5)\e[m \e[$below DNS Changer\e[m                 \e[$number (11)\e[m \e[$below TRAFFIC XRAY\e[m"
 echo -e   "  \e[$number (•6)\e[m \e[$below SYSTEM MENU\e[m                 \e[$number (12)\e[m \e[$below INFO ALL PORT\e[m"
+echo -e   "  \e[$number (100)\e[m \e[$below UPDATE\e[m                     >>>>>>>>>>>>>>>>>>" | lolcat
 echo -e   " \e[$line════════════════════════════════════════════════════════════\e[m"
-echo -e   "  \e[$number (100)\e[m \e[$below UPDATE\e[m"
-echo -e   ""
 echo -e   "  \e[$below 𝗩𝗲𝗿𝘀𝗶𝗼𝗻 :1.5"
 echo -e   "  \e[$below 𝐒𝐜𝐫𝐢𝐩𝐓 𝐁𝐲 𝐊𝐡𝐚𝐢 𝐕 𝐏 𝐍"
 echo -e   ""
