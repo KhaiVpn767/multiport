7fe89cb3812ca2eba837223fad771ff10dab0ce5
KhaiVpn767 <112326124+KhaiVpn767@users.noreply.github.com>
2025-11-06 23:35:14 +0800

    Update restoree.sh

--- system/restoree.sh
+++ system/restoree.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-# =========================================
-# Quick Setup | Script Setup Manager
-# Edition : Stable Edition V1.0
-# Auther  : KhaiVpn767
-# (C) Copyright 2022
-# =========================================
+#Autoscript-Lite By KhaiVpn767
 red='\e[1;31m'
 green='\e[0;32m'
 purple='\e[0;35m'
@@ -18,36 +13,59 @@ echo ""
 read -rp " Password File: " -e InputPass
 read -rp " Link File: " -e url
 wget -O backup.zip "$url"
-unzip -P $InputPass /root/backup.zip &> /dev/null
+unzip -P $InputPass /root/backup.zip &>/dev/null
 rm -f backup.zip
 sleep 1
-echo -e "[ ${green}INFO${NC} ] Start Restore . . . "
-cp -r /root/backup/.acme.sh /root/ &> /dev/null
-cp -r /root/backup/premium-script /var/lib/ &> /dev/null
-cp -r /root/backup/xray /usr/local/etc/ &> /dev/null
-cp -r /root/backup/public_html /home/vps/ &> /dev/null
-cp -r /root/backup/xray/ /usr/local/etc/ >/dev/null
-cp -r /root/backup/crontab /etc/ &> /dev/null
-cp -r /root/backup/cron.d /etc/ &> /dev/null
-cp -r /root/backup/shadow /etc/ &> /dev/null
-cp -r /root/backup/gshadow /etc/ &> /dev/null
-cp -r /root/backup/passwd /etc/ &> /dev/null
-cp -r /root/backup/group /etc/ &> /dev/null
+echo " Start Restore . . . "
+cp /root/backup/.acme.sh /root/ &>/dev/null
+echo -e "[ ${green}INFO${NC} ] Restoring passwd data..."
+sleep 1
+cp /root/backup/passwd /etc/ &>/dev/null
+echo -e "[ ${green}INFO${NC} ] Restoring group data..."
+sleep 1
+cp /root/backup/group /etc/ &>/dev/null
+echo -e "[ ${green}INFO${NC} ] Restoring shadow data..."
+sleep 1
+cp /root/backup/shadow /etc/ &>/dev/null
+echo -e "[ ${green}INFO${NC} ] Restoring chap-secrets data..."
+sleep 1
+cp /root/backup/chap-secrets /etc/ppp/ &>/dev/null
+echo -e "[ ${green}INFO${NC} ] Restoring passwd data..."
+sleep 1
+cp /root/backup/passwd1 /etc/ipsec.d/passwd &>/dev/null
+cp -r /root/backup/premium-script /var/lib/ &>/dev/null
+cp -r /root/backup/xray /usr/local/etc/ &>/dev/null
+cp -r /root/backup/public_html /home/vps/ &>/dev/null
+cp /root/backup/crontab /etc/ &>/dev/null
+cp -r /root/backup/cron.d /etc/ &>/dev/null
 rm -rf /root/backup
 rm -f backup.zip
 echo ""
 echo -e "[ ${green}INFO${NC} ] VPS Data Restore Complete !"
 echo ""
-echo -e "[ ${green}INFO${NC} ] Reboot in 5 second"
-systemctl restart nginx
+echo -e "[ ${green}ok${NC} ] Restarting XRAY Vmess WS"
 systemctl restart xray.service
-systemctl restart xray@none.service
+sleep 0.2
+echo -e "[ ${green}ok${NC} ] Restarting XRAY Vless WS"
 systemctl restart xray@vless.service
-systemctl restart xray@vnone.service
+sleep 0.2
+echo -e "[ ${green}ok${NC} ] Restarting XRAY Trojan WS"
 systemctl restart xray@trojanws.service
-systemctl restart xray@trnone.service
-systemctl restart xray@xtrojan.service
-systemctl restart xray@trojan.service
-service cron restart
-sleep 5
-reboot
+sleep 0.2
+echo -e "[ ${green}ok${NC} ] Restarting Nginx"
+/etc/init.d/nginx restart >/dev/null 2>&1
+sleep 0.2
+echo -e "[ ${green}ok${NC} ] Restarting Cron "
+/etc/init.d/cron restart >/dev/null 2>&1
+sleep 0.2
+echo -e "[ ${green}ok${NC} ] Restarting SSH "
+/etc/init.d/ssh restart >/dev/null 2>&1
+sleep 0.2
+echo -e "[ ${green}ok${NC} ] Restarting Dropbear "
+/etc/init.d/dropbear restart >/dev/null 2>&1
+sleep 0.2
+echo -e "[ ${green}ok${NC} ] Restarting stunnel4 "
+/etc/init.d/stunnel4 restart >/dev/null 2>&1
+sleep 0.2
+clear
+echo ""
