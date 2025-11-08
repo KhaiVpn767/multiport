2c1507c41c25e79b03e86ac4f19076e127f39c63
KhaiVpn767 <112326124+KhaiVpn767@users.noreply.github.com>
2025-11-08 22:34:04 +0800

    Update restore.sh

--- system/restore.sh
+++ system/restore.sh
@@ -1,32 +1,71 @@
 #!/bin/bash
-#wget https://github.com/${GitUser}/
-GitUser="KhaiVpn767"
-#IZIN SCRIPT
-MYIP=$(curl -sS ipv4.icanhazip.com)
+#Autoscript-Lite By KhaiVpn767
+red='\e[1;31m'
+green='\e[0;32m'
+purple='\e[0;35m'
+orange='\e[0;33m'
+NC='\e[0m'
 clear
-echo "This Feature Can Only Be Used According To Vps Data With This Autoscript"
-echo "Please input link to your vps data backup file."
-echo "You can check it on your email if you run backup data vps before."
-read -rp "Link File: " -e url
+echo ""
+echo " This Feature Can Only Be Used According To VPS Data With This Autoscript"
+echo " Please Insert VPS Data Backup Link To Restore The Data"
+echo ""
+read -rp " Password File: " -e InputPass
+read -rp " Link File: " -e url
 wget -O backup.zip "$url"
-unzip backup.zip
+unzip -P $InputPass /root/backup.zip &>/dev/null
 rm -f backup.zip
 sleep 1
-echo Start Restore
-cd /root/backup
-cp passwd /etc/
-cp group /etc/
-cp shadow /etc/
-cp gshadow /etc/
-cp -r wireguard /etc/
-cp ss.conf /etc/shadowsocks-libev/ss.conf
-cp -r premium-script /var/lib/
-cp -r xray /usr/local/etc/
-cp -r trojan-go /etc/
-cp -r shadowsocksr /usr/local/
-cp -r public_html /home/vps/
-cp crontab /etc/
-strt
+echo " Start Restore . . . "
+cp /root/backup/.acme.sh /root/ &>/dev/null
+echo -e "[ ${green}INFO${NC} ] Restoring passwd data..."
+sleep 1
+cp /root/backup/passwd /etc/ &>/dev/null
+echo -e "[ ${green}INFO${NC} ] Restoring group data..."
+sleep 1
+cp /root/backup/group /etc/ &>/dev/null
+echo -e "[ ${green}INFO${NC} ] Restoring shadow data..."
+sleep 1
+cp /root/backup/shadow /etc/ &>/dev/null
+echo -e "[ ${green}INFO${NC} ] Restoring chap-secrets data..."
+sleep 1
+cp /root/backup/chap-secrets /etc/ppp/ &>/dev/null
+echo -e "[ ${green}INFO${NC} ] Restoring passwd data..."
+sleep 1
+cp /root/backup/passwd1 /etc/ipsec.d/passwd &>/dev/null
+cp -r /root/backup/premium-script /var/lib/ &>/dev/null
+cp -r /root/backup/xray /usr/local/etc/ &>/dev/null
+cp -r /root/backup/public_html /home/vps/ &>/dev/null
+cp /root/backup/crontab /etc/ &>/dev/null
+cp -r /root/backup/cron.d /etc/ &>/dev/null
 rm -rf /root/backup
 rm -f backup.zip
-echo Done
+echo ""
+echo -e "[ ${green}INFO${NC} ] VPS Data Restore Complete !"
+echo ""
+echo -e "[ ${green}ok${NC} ] Restarting XRAY Vmess WS"
+systemctl restart xray.service
+sleep 0.2
+echo -e "[ ${green}ok${NC} ] Restarting XRAY Vless WS"
+systemctl restart xray@vless.service
+sleep 0.2
+echo -e "[ ${green}ok${NC} ] Restarting XRAY Trojan WS"
+systemctl restart xray@trojanws.service
+sleep 0.2
+echo -e "[ ${green}ok${NC} ] Restarting Nginx"
+/etc/init.d/nginx restart >/dev/null 2>&1
+sleep 0.2
+echo -e "[ ${green}ok${NC} ] Restarting Cron "
+/etc/init.d/cron restart >/dev/null 2>&1
+sleep 0.2
+echo -e "[ ${green}ok${NC} ] Restarting SSH "
+/etc/init.d/ssh restart >/dev/null 2>&1
+sleep 0.2
+echo -e "[ ${green}ok${NC} ] Restarting Dropbear "
+/etc/init.d/dropbear restart >/dev/null 2>&1
+sleep 0.2
+echo -e "[ ${green}ok${NC} ] Restarting stunnel4 "
+/etc/init.d/stunnel4 restart >/dev/null 2>&1
+sleep 0.2
+clear
+echo ""
