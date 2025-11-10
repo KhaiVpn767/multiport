ea300e2d852d6953de7ea730ae4b4c256f224928
KhaiVpn767 <112326124+KhaiVpn767@users.noreply.github.com>
2025-11-10 15:52:54 +0800

    Update restore.sh

--- system/restore.sh
+++ system/restore.sh
@@ -4,29 +4,36 @@ GitUser="KhaiVpn767"
 #IZIN SCRIPT
 MYIP=$(curl -sS ipv4.icanhazip.com)
 clear
-echo "This Feature Can Only Be Used According To Vps Data With This Autoscript"
-echo "Please input link to your vps data backup file."
-echo "You can check it on your email if you run backup data vps before."
-read -rp "Link File: " -e url
-wget -O backup.zip "$url"
-unzip backup.zip
-rm -f backup.zip
-sleep 1
-echo Start Restore
-cd /root/backup
-cp passwd /etc/
-cp group /etc/
-cp shadow /etc/
-cp gshadow /etc/
-cp -r wireguard /etc/
-cp ss.conf /etc/shadowsocks-libev/ss.conf
-cp -r premium-script /var/lib/
-cp -r xray /usr/local/etc/
-cp -r trojan-go /etc/
-cp -r shadowsocksr /usr/local/
-cp -r public_html /home/vps/
-cp crontab /etc/
-strt
-rm -rf /root/backup
-rm -f backup.zip
-echo Done
+bottoken=$(sed -n '1p' /root/.bckupbot | awk '{print $1}')
+    read -p "File Url   : " file_url
+    wget -O /root/backup.zip "$file_url"
+    echo -e "[ ${green}INFO${NC} ] • Restore Data..."
+    read -rp "Password File: " InputPass
+    echo -e "[ ${green}INFO${NC} ] • Getting your data..."
+    unzip -P $InputPass /root/backup.zip &>/dev/null
+    echo -e "[ ${green}INFO${NC} ] • Starting to restore data..."
+    rm -f /root/backup.zip &>/dev/null
+    sleep 1
+    cd /root/backup
+    echo -e "[ ${green}INFO${NC} ] • Please Wait , Restoring In Process Now... "
+    sleep 1
+    cp passwd /etc/ &>/dev/null
+    echo -e "[ ${green}INFO${NC} ] • Restoring passwd data..."
+    sleep 1
+    cp group /etc/ &>/dev/null
+    echo -e "[ ${green}INFO${NC} ] • Restoring group data..."
+    sleep 1
+    cp shadow /etc/ &>/dev/null
+    echo -e "[ ${green}INFO${NC} ] • Restoring shadow data..."
+    sleep 1
+    cp gshadow /etc/ &>/dev/null
+    echo -e "[ ${green}INFO${NC} ] • Restoring gshadow data..."
+    sleep 1
+    cp -r premium-script /var/lib/ &>/dev/null
+    cp -r xray /usr/local/etc/ &>/dev/null
+    cp -r public_html /home/vps/ &>/dev/null
+    rm -rf /root/backup
+    rm -f backup.zip
+    echo -e "[ ${green}INFO${NC} ] • Done Restore... "
+    read -n 1 -s -r -p "Press any key to back on menu"
+    menu
